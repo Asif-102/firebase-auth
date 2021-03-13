459b1db8b88575e38e09a56ea4e9e8ed661d129c
Asif2535 <aiqbal517@gmail.com>
2021-03-13 20:20:53 +0600

    learn firebase authentication process

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJlMcfwWoGCKYUPFYbUUDoDvxaFtFd8tw",
+    authDomain: "ema-john-project-simple.firebaseapp.com",
+    projectId: "ema-john-project-simple",
+    storageBucket: "ema-john-project-simple.appspot.com",
+    messagingSenderId: "290593725771",
+    appId: "1:290593725771:web:ff6a548e9e06cff7d11cce"
+  };
+
+  export default firebaseConfig;
