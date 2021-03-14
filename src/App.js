70e29ea385bf6e8bb42f2d105479559f0acb9233
Asif2535 <aiqbal517@gmail.com>
2021-03-14 21:03:30 +0600

    learned how to create sign up

--- src/App.js
+++ src/App.js
@@ -8,82 +8,157 @@ import { useState } from 'react';
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
-  }
+}
 
 // firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [newUser, setNewUser] = useState(false);
   const [user, setUser] = useState({
-    isSignedIn:false,
+    isSignedIn: false,
     name: '',
     email: '',
+    password: '',
     photo: ''
   });
 
   const provider = new firebase.auth.GoogleAuthProvider();
-  const handleSingIn = () =>{
+  const handleSingIn = () => {
     firebase.auth().signInWithPopup(provider)
-    .then(res => {
-      const {displayName, photoURL, email} = res.user;
-      const signedInUser = {
-        isSignedIn: true,
-        name:displayName,
-        email:email,
-        photo: photoURL
-      };
-      setUser(signedInUser);
-    })
-    .catch(err => {
-      console.log(err);
-      console.log(err.message);
-    })
+      .then(res => {
+        const { displayName, photoURL, email } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        };
+        setUser(signedInUser);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
   }
 
-  const handleSignOut = () =>{
+  const handleSignOut = () => {
     firebase.auth().signOut()
-    .then(res => {
-      const signedOutUser = {
-        isSignedIn: false,
-        name:'',
-        photo:'',
-        email:''
-      }
-      setUser(signedOutUser);
-    })
-    .catch(err => {
-      // An error happened.
+      .then(res => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          photo: '',
+          email: '',
+          error: '',
+          success: false
+        }
+        setUser(signedOutUser);
+      })
+      .catch(err => {
+        // An error happened.
+      });
+  }
+
+  const handleBlur = (e) => {
+    let isFieldValid = true;
+    if (e.target.name === 'email') {
+      isFieldValid = /\S+@\S+\.\S+/.test(e.target.value);
+    }
+    if (e.target.name === 'password') {
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber;
+    }
+    if (isFieldValid) {
+      // [...cart, newItem]
+      const newUserInfo = { ...user };
+      newUserInfo[e.target.name] = e.target.value;
+      setUser(newUserInfo);
+    }
+  }
+  const handleSubmit = (e) => {
+    // console.log(user.email, user.password);
+    if (newUser && user.email && user.password) {
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+        .then(res => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = '';
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+
+          console.log(res)
+        })
+        .catch(error => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+          updateUserName(user.name);
+        });
+    }
+
+    if (!newUser && user.email && user.password) {
+      firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+        .then(res => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = '';
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          console.log('sign in user info', res.user);
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          newUserInfo.success = false;
+          setUser(newUserInfo);
+        });
+    }
+
+    e.preventDefault();
+  }
+
+  const updateUserName = name => {
+    const user = firebase.auth().currentUser;
+
+    user.updateProfile({
+      displayName: name
+    }).then(function () {
+      console.log('user name updated successfully');
+    }).catch(function (error) {
+      console.log(error);
     });
-  }
-
-  const handleBlur = (event) =>{
-    console.log(event.target.name ,event.target.value);
-  }
-  const handleSubmit = () =>{
-
   }
 
   return (
     <div className="App">
       {
-        user.isSignedIn ?<button onClick={handleSignOut}>Sign out</button>:
-        <button onClick={handleSingIn}>Sign in</button>
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign out</button> :
+          <button onClick={handleSingIn}>Sign in</button>
       }
+      <br/>
+      <button>Sign in Using Facebook</button>
       {
         user.isSignedIn && <div>
           <p> Welcome, {user.name}</p>
           <p>Your email: {user.email}</p>
-          <img src={user.photo} alt=""/>
+          <img src={user.photo} alt="" />
         </div>
       }
 
       <h1>Our own Authentication</h1>
+      <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
+      <label htmlFor="newUser">New User Sign up</label>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="email" onBlur={handleBlur} placeholder="Your Email address" required/>
-        <br/>
-        <input type="password" name="password" onBlur={handleBlur} placeholder="Your Password" required/>
-        <br/>
-        <input type="submit" value="Submit"/>
+        {newUser && <input name="name" type="text" onBlur={handleBlur} placeholder="Your name" />}
+        <br />
+        <input type="text" name="email" onBlur={handleBlur} placeholder="Your Email address" required />
+        <br />
+        <input type="password" name="password" onBlur={handleBlur} placeholder="Your Password" required />
+        <br />
+        <input type="submit" value={newUser  ? 'Sign up':'Sign in'}/>
       </form>
+      <p style={{ color: 'red' }}>{user.error}</p>
+      {user.success && <p style={{ color: 'green' }}>User {newUser ? 'created' : 'Logged In'} successfully</p>}
     </div>
   );
 }
